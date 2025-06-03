66281aa1b45c6c9fe4f4835afd42a13868ee0494
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-06-03 10:00:14 +0700

    first commit

---
+++ abstraksi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class seseorang {
+    public:
+    virtual void pesan(){
+        cout<<"pesan dari seseorang"<<endl;
+    }
+};
+
+class joko :public seseorang{
+    public:
+    void pesan(){
+        cout << "pesan dari joko" << endl;
+    }
+};
